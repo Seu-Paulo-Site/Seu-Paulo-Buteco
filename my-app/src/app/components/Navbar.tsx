dbf085b18a8f3519f557f8ccd7b8cc047b29fa13
Vircsss <virc06@gmail.com>
2025-07-25 02:55:44 -0300

    Realizando os primeiros comandos do react

---
+++ my-app/src/app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+export default function Navbar() {
+  return (
+    <nav className=''>
+      <img src="#" alt="Logo do Seu paulo boteco" />
+      <div>
+        <ul>
+          <li>HOME</li>
+          <li>HÍSTORIAS</li>
+          <li>DIAS E HORÁRIOS</li>
+          <li>CARDÁPIOS</li>
+          <li>HORÁRIOS</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
