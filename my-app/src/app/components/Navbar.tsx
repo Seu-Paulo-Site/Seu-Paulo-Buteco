ee4237389b4f4996ef3d47c6d5e3a5e8405fa662
Vircsss <virc06@gmail.com>
2025-07-25 15:23:45 -0300

    atualizando nav bar

--- my-app/src/app/components/Navbar.tsx
+++ my-app/src/app/components/Navbar.tsx
@@ -3,11 +3,11 @@
 export default function Navbar() {
   return (
     <nav className="ml-10 flex justify-center w-[90%] h-16 bg-white ">
-      <div className="flex  items-center justify- ">
+      <div className="flex  items-center justify-between ">
         <img src="/seuButeco img.png" alt="Logo do Seu Paulo Boteco" className="h-14 w-18 bg-white" />
 
-        <ul className="flex gap-6 text-red-800 font-semibold ">
-          <li>HOME</li>
+        <ul className="flex gap-6 text-black font-semibold ">
+          <li className="text-red-800">HOME</li>
           <li>HÍSTORIAS</li>
           <li>DIAS E HORÁRIOS</li>
           <li>CARDÁPIOS</li>
